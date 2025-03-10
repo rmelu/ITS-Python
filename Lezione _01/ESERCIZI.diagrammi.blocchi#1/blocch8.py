75f1628377cc7656ec75420bc7a7618cfd6e47a2
rmelu <m.riccia@yahoo.com>
2025-03-10 15:54:52 +0100

    esercizi aggiornati

--- Lezione _01/ESERCIZI.diagrammi.blocchi#1/blocch8.py
+++ Lezione _01/ESERCIZI.diagrammi.blocchi#1/blocch8.py
@@ -1,17 +1,15 @@
 #leggi la soglia dell'utente
 soglia =int(input("Inserisci il valore della sogloia: "))
-#inizia il contatore
-cont = 0
-#inizia il ciclo while
-while cont != 7:
+
+#interazione per 7 volte
+for cont in range (7) :
     #leggi un numero dall'utente
-    n = int(input("Inserisci numero: "))
+    n = float(input("Inserisci numero: "))
     #controlla se il numero è maggiore della soglia
     if n > soglia:
             #se si, stampa numero
-            print(n)
-            #incrementa il contatore
-            cont += 1
+            print(f"numero maggiore della soglia: {n}")
+
  #fine del programma           
 print("Fine del programma")
 
--- Lezione _01/ESERCIZI.diagrammi.blocchi#1/blocchi9.py
+++ Lezione _01/ESERCIZI.diagrammi.blocchi#1/blocchi9.py
@@ -5,9 +5,8 @@ max__nome = nome
 max_vendite = vendite
 min_nome = nome
 min_vendite = vendite
-cont = 0
-#ciclo principale
-while cont < 5:
+#ciclo forper elaborazione 20 coppie nome-vendite
+for i in range(19):
     nuovo_nome = input("Inserisci il nome: ")
     nuovo_vendite = float(input("Inserisci le nuove vendite: "))
 
@@ -18,9 +17,7 @@ while cont < 5:
     if nuovo_vendite < min_vendite:
         min_nome = nuovo_nome
         min_vendite = nuovo_vendite
-        cont += 1
+
   #stampa i risultati      
-print("Nome con vendite massime:", max_nome)
-print("Vendite massime:", max_vendite)
-print("Nome con vendite minime:", min_nome)
-print("Vendite minime:", min_vendite)
+print("Venditore con vendite massime:", max__nome, "Vendite:", max_vendite)
+print("Venditore con vendite minime:", min_nome, "Vendite:", min_vendite)
